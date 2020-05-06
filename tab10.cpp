f72ee89ce4e46b64e767ce70943cfdd005ff058d
AdrianHaziak <62154194+AdrianHaziak@users.noreply.github.com>
2020-05-06 10:30:02 +0200

    Add files via upload

---
+++ tab10.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<cstdlib>
+#include<fstream>
+using namespace std;
+
+int main()
+{
+	ifstream wej;
+	ofstream wyj;
+	
+	wej.open ("tab10.txt");
+	wyj.open ("wyjTab10.json");
+	
+	int t[10]
+		if(wej.good()) {
+			while(!wej.eof())
+			for(int i=1;i<10;i++)
+			wej>>t[i;]
+			}
+		
+	int max=t[0],
+	
+	max = tab[0]; 	
+	for(int i=1;i<10;i++) 
+		if(max<tab[i])
+			max=tab[i];	
+			
+		wej<<" [ \n { \n \"najwieksza\" : \""<<naj]"	
+	
+	wej.close();
+	wyj.close();
+	system("pause");
+	return 0;
+}
