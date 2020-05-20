16be12db2c33f9aaef4eb0838a90fc7f1e4f6c91
AdrianHaziak <62154194+AdrianHaziak@users.noreply.github.com>
2020-05-20 10:51:13 +0200

    Add files via upload

---
+++ tabson.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+    int t[100];
+
+    for(int i=0; i<100; i++){
+        if(zm==t[i])
+            return false;
+    }
+
+int main()
+{
+    ifstream wej;
+    ifstream wyj;
+
+    wej.open("wej.json");
+    wyj.open("wyj.json");
+    int li = 0;
+        if(wej.good()){
+            while(!wej.eof())
+
+            for(int i = 0; i<100; i++){
+                    if(i==li
+                wej>>t[i];
+                wej<<" [ \n { \n "li" :" " << i;
+            }
+                li++;
+        }
+    return 0;
+}
